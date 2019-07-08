9cfdffb1458f6eb11ae2e686cb4f664b6dc4693e
Tom Underman <tomunderman@Toms-MacBook-Pro.local>
2019-07-08 15:59:13 -0400

    First commit

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+    screams:[
+        {
+            userHandle: "user",
+             body: "this is the body",
+             createdAt: "time",
+             likeCount: 5,
+             commentCount: 2
+
+        }
+    ]
+}
